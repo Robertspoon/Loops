6fe2996fb6b2afbde65cb26a7e4b90bb2c049a90
Robertspoon <robertthemcjug@gmail.com>
2022-10-31 10:42:51 -0300

    Initial commit

---
+++ Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loops");
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
