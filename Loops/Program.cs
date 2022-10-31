41d21ebedc760d32b973829d2ac631d83ae7863a
Robertspoon <robertthemcjug@gmail.com>
2022-10-31 11:06:21 -0300

    poijhyuf

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -13,6 +13,26 @@ namespace Loops
             Console.WriteLine("Loops");
             Console.WriteLine();
 
+            int[] ammo = new int[5];
+            ammo[0] = 6;
+            ammo[1] = 12;
+            ammo[2] = 2;
+            ammo[3] = 35;
+            ammo[4] = 7;
+
+            int start = 0;
+            int end = 4;
+
+            int i = start;
+
+            // count from starts...end
+
+            while (i <= end)
+            {
+                Console.Write(ammo[i] + ", ");
+                i = i + 1;
+            }
+
             Console.ReadKey(true);
         }
     }
